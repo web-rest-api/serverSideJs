9f7331a4e666d2836e5fb0e8ef69567bb3f934f0
Armand <armand.vieau@epita.fr>
2026-04-30 17:17:09 +0200

    MongoDB Student services

---
+++ services/studentServiceMongoDB.js
@@ -0,0 +1,30 @@
+import bcrypt from "bcrypt";
+import User from "../models/userModel.js";
+
+const SALT_ROUNDS = 10;
+
+export const findAllUsers = () => User.find();
+
+export const findStudentById = (id) => User.findById(id);
+
+export const createStudentService = async (newStudent) => {
+  const hashedPassword = await bcrypt.hash(newStudent.password, SALT_ROUNDS);
+  return User.create({ ...newStudent, password: hashedPassword });
+};
+
+export const loginStudentService = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("Invalid credentials");
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) throw new Error("Invalid credentials");
+  return user;
+};
+
+export const deleteStudentService = (id) => User.findByIdAndDelete(id);
+
+export const updateStudentService = async (id, newStudent) => {
+  if (newStudent.password) {
+    newStudent.password = await bcrypt.hash(newStudent.password, SALT_ROUNDS);
+  }
+  return User.findByIdAndUpdate(id, newStudent);
+};
