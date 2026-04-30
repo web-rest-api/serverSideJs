98fa17c55a1d77cf5c2a779af300f4b18b06d6cc
Armand <armand.vieau@epita.fr>
2026-04-30 17:17:30 +0200

    Service moved to mongo db file

--- services/studentsService.js
+++ services/studentsService.js
@@ -1,47 +1,32 @@
-import { students } from "../students.js";
+// emulates db connection !!
+// import { students } from "../data/students.js";
+
+const students = [];
 
 export const findAllUsers = () => {
-    if (!students || students.length === 0) {
-        throw new Error("No users found")
-    } else {
-        return students
-    }
-}
+  if (!students || students.length === 0) {
+    throw new Error("No users found");
+  } else {
+    return students;
+  }
+};
 
 /**
- * @param {*} id :string
- * @returns
+ * @param {*} id string
+ * @returns student
  */
 
 export const findUser = (id) => {
-    const foundStudent = students.find((student) => student.id === id)
-    if (foundStudent) {
-        return foundStudent
-    } else {
-        throw new Error("User not found")
-    }
-}
+  const foundStudent = students.find((student) => student.id === id);
 
-// add middleware checks !!!!! 🚨‼️
+  if (foundStudent) {
+    return foundStudent;
+  } else {
+    throw new Error("User not found");
+  }
+};
+
+// add middleware checks !!!!!
 export const createStudentService = (newStudent) => {
-    students.push(newStudent)
-}
-
-export const updateStudentService = (id, updatedData) => {
-    const index = students.findIndex((student) => student.id === id)
-    if (index === -1) {
-        throw new Error("User not found")
-    }
-    for (let key in updatedData) {
-        students[index][key] = updatedData[key]
-    }
-    return students[index]
-}
-
-export const deleteStudentService = (id) => {
-    const index = students.findIndex((student) => student.id === id)
-    if (index === -1) {
-        throw new Error("User not found")
-    }
-    return students.splice(index, 1)[0]
-}
+  students.push(newStudent);
+};
