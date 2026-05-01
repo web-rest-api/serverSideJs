2e59d40001abf289234c55e8ed32668d59abfa30
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-05-02 00:39:53 +0200

    feat: paths set

--- BACK/routes/studentsRoute.js
+++ BACK/routes/studentsRoute.js
@@ -1,4 +1,11 @@
 import express from "express";
+import {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+} from "../controllers/studentsController.js";
 
 // TODO 1: Import getAllStudents, getStudentById, createStudent, updateStudent, deleteStudent
 //         from ../controllers/studentsController.js
@@ -6,10 +13,20 @@ import express from "express";
 const studentRouter = express.Router();
 
 // TODO 2: Wire up the routes:
+
 //   GET    /        → getAllStudents
+studentRouter.get("/", getAllStudents);
+
 //   GET    /:id     → getStudentById
+studentRouter.get("/:id", getStudentById);
+
 //   POST   /        → createStudent
+studentRouter.post("/", createStudent);
+
 //   PUT    /:id     → updateStudent
+studentRouter.put("/", createStudent);
+
 //   DELETE /:id     → deleteStudent
+studentRouter.delete("/", deleteStudent);
 
 export default studentRouter;
